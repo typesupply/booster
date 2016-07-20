7e00a4d57b13dd30744c746ab3e9f9eadcffaea1
Erik <erik@letterror.com>
2016-07-20 20:10:03 +0200

    setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name = "AppExt",
+      version = "0.1",
+      description = "RF Extension Framework.",
+      author = "Tal Leming",
+      author_email = "tal@typesupply.com",
+      url = "https://github.com/typesupply/appext",
+      license = "Private",
+      packages = [
+              "appext",
+      ],
+)
